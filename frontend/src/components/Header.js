e7218718f9ae7fab1630b4a3a88a1c22182af0e5
Nikita Koorgaonkar <nikita.kor011@gmail.com>
2023-03-21 14:44:31 -0700

    Adding header and search

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
+
+const navbarStyle ={
+    backgroundColor : 'lightblue'
+};
 
 const Header = ({ title }) => {
     return (
-    <Navbar position = "fixed" bg="light" variant="light">
-      <Navbar.Brand href="/">{title}</Navbar.Brand>
+    <Navbar style = {navbarStyle} position = "fixed">
+        <Container>
+            <Navbar.Brand href="/">{title}</Navbar.Brand>
+        </Container>
+      
   </Navbar>
 )
 };
 
-export default Header
+export default Header;
