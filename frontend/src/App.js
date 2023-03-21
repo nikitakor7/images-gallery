0734816258e30b76528ba6303b5820466fb09760
Nikita Koorgaonkar <nikita.kor011@gmail.com>
2023-03-20 19:49:49 -0700

    Adding header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header.js';
+
 function App() {
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title="Images Gallery"/>
     </div>
   );
 }
