cf515fe8f7c405893ef1db24ab74cced22272001
Nikita Koorgaonkar <nikita.kor011@gmail.com>
2023-03-23 18:10:59 -0700

    Display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,52 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header.js';
-import Search from './components/Search.js';
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header.js";
+import Search from "./components/Search.js";
+import ImageCard from "./components/ImageCard.js";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState("");
-  const [image, setImages] = useState("");
-  console.log(image);
-  
-  function handleSearchSubmit(e){
+  const [images, setImages] = useState("");
+
+  function handleSearchSubmit(e) {
     e.preventDefault();
-    console.log(word);
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
-    .then((res) => res.json())
-    .then((data)=> {
-      setImages([data, ... image]);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  };
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([{ ...data, title: word }, ...images]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
+  function handleDeleteImage(id){
+    function doCompare(image) {
+      return id != image.id;
+    }
+    setImages(images.filter(doCompare));
+  }
   console.log(process.env);
   return (
-    <div className='App'>
+    <div className="App">
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {!!images.length && images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard key={i} image={image} deleteImage={handleDeleteImage}/>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
-
 }
 
 export default App;
